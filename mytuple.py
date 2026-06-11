63ddc78bff972649ff10980d21c2dadf9a9e4094
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2026-06-11 09:52:05 +0300

    Add doctest, fix __init__, __new__
    TupleMeta
    -    def __init__(cls, bases, clsdict):
    +    def __init__(cls, clsname, bases, clsdict):
    MyTuple
    -        return super().__new__(args)
    +        return super().__new__(cls, args)

--- mytuple.py
+++ mytuple.py
@@ -1,11 +1,23 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # PYTHON_ARGCOMPLETE_OK
+"""
+>>> bob = Person("Bob", 37, 28000)
+>>> bob
+('Bob', 37, 28000)
+>>> bob.name, bob.age, bob.salary
+('Bob', 37, 28000)
+>>> alice = Person("Alice", 16)
+Traceback (most recent call last):
+...
+TypeError: <class '__main__.Person'> expect 3 arguments
+>>>
+"""
 from operator import itemgetter
 
 
 class TupleMeta(type):
-    def __init__(cls, bases, clsdict):
+    def __init__(cls, clsname, bases, clsdict):
         fields = clsdict.get("_fields", [])
         for n, name in enumerate(fields):
             setattr(cls, name, property(itemgetter(n)))
@@ -15,7 +27,7 @@ class MyTuple(tuple, metaclass=TupleMeta):
     def __new__(cls, *args):
         if (n := len(cls._fields)) != len(args):
             raise TypeError(f"{cls} expect {n} arguments")
-        return super().__new__(args)
+        return super().__new__(cls, args)
 
 
 class Person(MyTuple):
@@ -23,5 +35,9 @@ class Person(MyTuple):
 
 
 if __name__ == "__main__":
-    bob = Person("Bob", 37, 28000)
-    print(bob)
+
+    # bob = Person("Bob", 37, 28000)
+    # print(bob)
+    import doctest
+
+    doctest.testmod()
